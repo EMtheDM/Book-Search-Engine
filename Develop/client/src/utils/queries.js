ce54d1b2f41cd8b68558df72eac500fd7e6ae26e
Eric Martin <eric_martin@me.com>
2022-02-05 19:49:11 -0700

    added me query to queries.js

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -2,23 +2,19 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
     query me {
-        user {
-        _id
-        username
-        email
-        bookCount
-        savedBooks
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
         }
     }
 `;
-
-export const GET_USERS = gql`
-    query users {
-    
-    _id
-    username
-    email
-    bookCount
-    savedBooks
-    }
-`
